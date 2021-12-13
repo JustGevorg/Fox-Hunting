237c53d0df327f7cf7f8b88f8b82131e1dd8e0ed
Gevorg <gevorg2310@yandex.ru>
2021-12-13 23:57:35 +0300

    All changes added into Documentation.txt

--- main.pl
+++ main.pl
@@ -1,62 +1,102 @@
 %%%%%%%%%%%%%%%%% Fox Hunting %%%%%%%%%%%%%%%%%%
 
+% Making foxes coordinates's list
 random_list(N, T, L) :-
-    findall([X,Y], (between(1,N,_), random(0, T,X), random(0, T, Y)), L).
+    findall([X,Y], (between(1,N,_), random(0, T, X), random(0, T, Y)), L).
 
-vert_catch([_, V], [_, V]) :- writeln("Fox on vertical").
-hor_catch([H, _], [H, _]) :- writeln("Fox on horizontal").
+% Checking foxes at horizontal and vertical
+vert_catch([V, _], [V, _], _) :- writeln("Fox on horizontal").
+hor_catch([_, H], [_, H], _) :- writeln("Fox on vertical").
 
-diag_catch([H, V], [H1, V1]) :-
-    V>V1, H<H1,
-    diag_catch_up_right([H, V], [H1, V1]), 
+% Checking foxes at diagonal, 
+% search direction depends of hunter and fox interposition
+diag_catch([V, H], [V1, H1], Size) :-
+    V>V1, H<H1 ->
+    diag_catch_up_right([V, H], [V1, H1], Size), 
     writeln("Fox on diagonal"),!;
-    V>V1, H>H1,
-    diag_catch_up_left([H, V], [H1, V1]), 
+    V>V1, H>H1 ->
+    diag_catch_up_left([V, H], [V1, H1], Size), 
     writeln("Fox on diagonal"), !;
-    V<V1, H<H1,
-    diag_catch_down_right([H, V], [H1, V1]), 
+    V<V1, H<H1 ->
+    diag_catch_down_right([V, H], [V1, H1], Size), 
     writeln("Fox on diagonal"), !;
-     V<V1, H>H1,
-    diag_catch_down_left([H, V], [H1, V1]), 
+    V<V1, H>H1 ->
+    diag_catch_down_left([V, H], [V1, H1], Size), 
     writeln("Fox on diagonal"), !.
-%diag_catch([H, V], [H1, V1]) :-
-%    V>V1, H>H1,
-%    diag_catch_up_left([H, V], [H1, V1]), !.
-%diag_catch([H, V], [H1, V1]) :-
-%    V<V1, H<H1,
-%    diag_catch_down_right([H, V], [H1, V1]), !.
-%diag_catch([H, V], [H1, V1]) :-
-%    V<V1, H>H1,
-%    diag_catch_down_left([H, V], [H1, V1]), !.
 
-diag_catch_up_right([H, V], [H, V]) :- !.
-diag_catch_up_right([H, V], [H1, V1]) :- 
+% Serching in different direcions
+diag_catch_up_right([V, H], [V, H], _) :- !.
+diag_catch_up_right([V, H], [V1, H1], Size) :-
+    NewV is V-1, 
     NewH is H+1,
-    NewV is V-1,
-    diag_catch_up_right([NewH,NewV], [H1, V1]).
+    NewH < Size,
+    NewV > 0,
+    diag_catch_up_right([NewV,NewH], [V1, H1], Size).
 
-diag_catch_up_left([H, V], [H, V]) :- !.
-diag_catch_up_left([H, V], [H1, V1]) :- 
+diag_catch_up_left([V, H], [V, H], _) :- !.
+diag_catch_up_left([V, H], [V1, H1], Size) :- 
+    NewV is V-1,
     NewH is H-1,
-    NewV is V-1,
-    diag_catch_up_right([NewH,NewV], [H1, V1]).
+    NewH > 0,
+    NewV > 0,
+    diag_catch_up_right([NewV,NewH], [V1, H1], Size).
 
-diag_catch_down_right([H, V], [H, V]) :- !.
-diag_catch_down_right([H, V], [H1, V1]) :- 
+diag_catch_down_right([V, H], [V, H], _) :- !.
+diag_catch_down_right([V, H], [V1, H1], Size) :- 
+    NewV is V+1,
     NewH is H+1,
-    NewV is V+1,
-    diag_catch_down_right([NewH,NewV], [H1, V1]).
+    NewH < Size,
+    NewV < Size,
+    diag_catch_down_right([NewV,NewH], [V1, H1], Size).
 
-diag_catch_down_left([H, V], [H, V]) :- !.
-diag_catch_down_left([H, V], [H1, V1]) :- 
+diag_catch_down_left([V, H], [V, H], _) :- !.
+diag_catch_down_left([V, H], [V1, H1], Size) :- 
+    NewV is V+1,
     NewH is H-1,
-    NewV is V+1,
-    diag_catch_down_right([NewH,NewV], [H1, V1]).
+    NewH > 0,
+    NewV < Size,
+    diag_catch_down_right([NewV,NewH], [V1, H1], Size).
 
-catch([H, V], [H1, V1]) :-
-    vert_catch([H, V], [H1, V1]),!;
-    hor_catch([H, V], [H1, V1]),!;
-    diag_catch([H, V], [H1, V1]).
+% Start searching in all directions
+catching([V, H], [V1, H1], Size) :-
+    vert_catch([V, H], [V1, H1], Size),!;
+    hor_catch([V, H], [V1, H1], Size),!;
+    diag_catch([V, H], [V1, H1], Size).
 
-start_game() :-
-    writeln("Enter field size and foxes count").
+
+get_head([H|_], H).
+get_tail([_|T], T).
+
+
+fox_here([V, H], [V, H], 1) :- !, writeln("Fox catched"). % returns 1 if fox and hunter in same cell
+fox_here(_, _, 0) :- !, writeln("Fox not catched"). % returns 0 in any other case
+
+
+% Counts foxes at all directions from hunter
+fox_iteration(_, [], _, 0) :- !.
+fox_iteration([V, H], [FirstFox|OtherFoxes], Size, Answer) :- 
+    catching([V,H], FirstFox, Size) -> fox_iteration([V, H], OtherFoxes, Size, NextAnswer), Answer is NextAnswer+1;
+    fox_iteration([V, H], OtherFoxes, Size, Answer).
+
+% Here must be predicate to define catched fox
+%fox_right_here(_, [], _, 0) :- !.
+%fox_right_here([V, H], [FirstFox|OtherFoxes], Size, Answer) :- 
+%    fox_here([V,H], FirstFox, Param) -> ;
+%    fox_right_here([V, H], OtherFoxes, Size, Answer).
+
+%tttt(Hunter, [H1|T1]) :- fox_here(Hunter, H1)
+
+% Start of the game
+start :- 
+    write("Enter field size: "), nl, 
+    read(FieldSize),
+    write("Enter count of foxes: "), nl, 
+    read(Foxes),
+    random_list(Foxes, FieldSize, ListOfFoxes),
+    writeln(ListOfFoxes),
+    repeat,
+        writeln("Enter hunter position [V, H]"),
+        read(HunterPosition), nl,
+        (false ->
+        writeln(HunterPosition),
+        fail).
